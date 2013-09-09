7c0a107ada0fceee1a7674a93baf2bb5df333115
Peter Bengtsson <mail@peterbe.com>
2013-09-08 19:15:39 -0700

    some temporary changes

--- stephanie/main/context_processors.py
+++ stephanie/main/context_processors.py
@@ -10,15 +10,14 @@ def base(request):
 
     navs = [
         {'url': reverse('main:home'), 'title': 'Home'},
-        {'url': reverse('main:artgroup', args=('drawings',)), 'title': 'Drawings'},
-        {'url': reverse('main:artgroup', args=('photography',)), 'title': 'Photography'},
-        {'url': reverse('main:artgroup', args=('paintings',)), 'title': 'Paintings'},
+        #{'url': reverse('main:artgroup', args=('drawings',)), 'title': 'Drawings'},
+        #{'url': reverse('main:artgroup', args=('photography',)), 'title': 'Photography'},
+        #{'url': reverse('main:artgroup', args=('paintings',)), 'title': 'Paintings'},
         {'url': reverse('main:about'), 'title': 'About'},
         {'url': reverse('main:contact'), 'title': 'Contact'},
     ]
     for nav in navs:
         active = False
-#        print "COMPARE", (request.path, nav['url'])
         if request.path == nav['url']:
             active = True
         nav['active'] = active
