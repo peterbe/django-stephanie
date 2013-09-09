5836f0b13e436d6b2b4fa129982abb5ae72b1f03
Peter Bengtsson <mail@peterbe.com>
2013-09-09 09:02:06 -0700

    Revert "change how static urls are referenced"
    This reverts commit e96900955560c9e24abba125928a530565dc1923.

--- stephanie/settings/base.py
+++ stephanie/settings/base.py
@@ -70,7 +70,7 @@ STATIC_ROOT = os.path.join(HERE, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
